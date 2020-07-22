fb45f2bbdf738dcab9afbeddf8b624123d29b17a
Yevgeniy Zhitomirskiy <yevgen93z@gmail.com>
2020-07-22 08:34:25 -0400

    Update okta_api.sh

--- okta_api.sh
+++ okta_api.sh
@@ -1,30 +1,47 @@
 #!/bin/bash
-api_token="XXXXXXXXXXXXXXXXXXX"
 
-# This script adds users from the OKTA1-coretech group into the OKTA2-coretech group.
-# emails_in_OKTA1_coretech.csv contains one email per line delimited by a comma of each user in the OKTA1-coretech group.
+# This script adds users from OKTA1 group into OKTA2 group.
+OKTA1_api_token=XXXXXXXXXXXXXXXX
+OKTA2_api_token=XXXXXXXXXXXXXXXX
 
-# feed the emails one by one to the Okta API GET request and obtain each user's data from the OKTA2 side
+# Read parameters for the IDs of OKTA1 group and OKTA2 group
+OKTA1_GROUP=$1
+OKTA2_GROUP=$2
+
+# Send an Okta API GET request to get all the users from the OKTA1 group and save to JSON.
+curl -v -X GET \
+-H "Accept: application/json" \
+-H "Content-Type: application/json" \
+-H "Authorization: SSWS ${OKTA1_api_token}" \
+"https://nielsennmc.okta.com/api/v1/groups/${OKTA1_GROUP}/users" >> OKTA1_users_in_group.json
+
+# Parse the JSON output and return a csv file with each user's email per line delimited by a comma
+cat OKTA1_users_in_group.json | jq '.[].profile.email' | sed 's/^"//' | sed 's/"$/,/' > OTKA1_emails_in_group.csv
+
+# Feed the emails one by one to the Okta API GET request and obtain each user's data from the OKTA2 side
 while IFS=, read -r email
 do
     curl -v -X GET \
     -H "Accept: application/json" \
     -H "Content-Type: application/json" \
-    -H "Authorization: SSWS ${api_token}" \
-    "https://okta-domain.okta.com/api/v1/users?q=${email}" >> OKTA2_user_data.txt
+    -H "Authorization: SSWS ${OKTA2_api_token}" \
+    "https://nielsen.okta.com/api/v1/users?q=${email}" >> OKTA2_user_data.txt
     echo "" >> OKTA2_user_data.txt
-done < emails_in_OKTA1_coretech.csv
+done < OTKA1_emails_in_group.csv
 
 # grab only the UID from each line of OKTA2_user_data.txt
 # add a comma to the end of each line for the CSV to be delimited
 cut -c 9-28 OKTA2_user_data.txt | sed 's/$/,/' > OKTA2_uids.csv
 
-# feed the UIDs to the Okta API PUT request and add each UID to the OKTA2-coretech group 00g1j6yka1aPYWNBj0h8
+# feed the UIDs to the Okta API PUT request and add each UID to the OKTA2 group
 while IFS=, read -r uid
 do
     curl -v -X PUT \
     -H "Accept: application/json" \
     -H "Content-Type: application/json" \
-    -H "Authorization: SSWS ${api_token}" \
-    "https://okta-domain.okta.com/api/v1/groups/00g1j6yka1aPYWNBj0h8/users/$uid"
+    -H "Authorization: SSWS ${OKTA2_api_token}" \
+    "https://nielsen.okta.com/api/v1/groups/${OKTA2_GROUP}/users/$uid"
 done < OKTA2_uids.csv
+
+# Cleanup
+rm *.csv *.txt *.json
